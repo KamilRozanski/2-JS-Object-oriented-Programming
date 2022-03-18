a84575a7b11861b13d1540b480d8ab70218b5cc6
KamilRozanski <motomc1@gmail.com>
2022-03-18 14:51:09 +0100

    add method added

--- dynamicSwitch-easy/main.js
+++ dynamicSwitch-easy/main.js
@@ -5,27 +5,27 @@ class Switch {
     conditions = [];
 
     add(condition, callback) {
-        this.cases.push(callback)
         this.conditions.push(condition)
-        return condition ? console.log(true) : callback()
-    }
-
-    isValid() {
-
-        for (const condition of this.conditions) {
-            return !condition ? true : console.log("isValid")
+        if (condition) {
+            return callback()
         }
     }
 
-
+    isValid() {
+        for (const condition of this.conditions) {
+            console.log(condition, "condition")
+            return !condition ? true : console.log("isvalid")
+        }
+    }
 }
 
 const formChecker = new Switch();
 const value = "test";
 
-formChecker.add(value.length < 10, () => {
-    console.log("error")
+formChecker.add(value.length < 5, () => {
+    console.log("input is to short")
 })
-formChecker.add(value === 10, () => {
-    console.log("This is not a number 10")
+formChecker.add(value !== "Kamil", () => {
+    console.log("The input is not a Kamil")
 })
+console.log(formChecker.isValid())
