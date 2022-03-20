26165f22f5d05e7e2773d37b2dde63e1fa6a21ce
KamilRozanski <motomc1@gmail.com>
2022-03-20 13:57:15 +0100

    fist implementation added

--- dynamicSwitch-easy/main.js
+++ dynamicSwitch-easy/main.js
@@ -6,30 +6,19 @@ class Switch {
 
     add(condition, callback) {
         this.conditions.push(condition)
-        // console.log(`${this.conditions} conditions array`)
-        if (condition) {
-            return callback()
-        }
+        this.cases.push(callback)
     }
 
     isValid() {
-        for (const condition of this.conditions) {
-            // console.log(condition, "condition")
-            if (!condition) {
-                return true
-            } else {
-                this.conditions = []
-                this.isEmpty()
-            }
-        }
+        return this.conditions.reduce((acc, el, index) => {
+            el === true ? this.cases[index]() : false
+        }, 0)
     }
-
     isEmpty() {
+        // console.log(this.conditions)
         return this.conditions.length <= 0
     }
 }
-
-
 
 const formChecker = new Switch();
 const value = "test";
@@ -40,6 +29,7 @@ formChecker.add(value.length < 5, () => {
 formChecker.add(value !== "Kamil", () => {
     console.error("The input is not a Kamil")
 })
+
 console.log(formChecker.isEmpty() + " isEmpty") // false
 console.log(formChecker.isValid() + " isValid"); // === false
-console.group(formChecker.isEmpty() + " isEmpty") // === true
+console.log(formChecker.isEmpty() + " isEmpty") // === true
