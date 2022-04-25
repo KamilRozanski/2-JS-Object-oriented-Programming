7f1fe189676e1beef171e640b44389e419c39975
KamilRozanski <motomc1@gmail.com>
2022-04-25 22:14:32 +0200

    I added new class Group

--- addressBook-medium/addressBook.js
+++ addressBook-medium/addressBook.js
@@ -1,7 +1,7 @@
 import {
     v4 as uuidv4
 } from 'uuid';
-// let id = uuidv4()
+
 
 
 
@@ -10,10 +10,14 @@ class Contact {
     // Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji i utworzenia, uuid
     // Ma umożliwiać: aktualizację datę modyfikacji, pozwalac na modyfikację imienia, nazwiska oraz adresu email
     constructor(name, lastName, email) {
+        const date = () => {
+            return new Date()
+        }
+
         this.name = name
         this.lastName = lastName
         this.email = email
-        this.creationDate = new Date()
+        this.creationDate = `${date().getDate()}/${date().getMonth() + 1}/${date().getFullYear()}`
         this.modificationDate = false
         this.id = uuidv4()
     }
@@ -22,6 +26,7 @@ class Contact {
         this.name = value
         this.modificationDate = new Date()
     }
+
     changeLastName = (value) => {
         this.lastName = value
         this.modificationDate = new Date()
@@ -33,9 +38,6 @@ class Contact {
 }
 const contact = new Contact("kamil", "rozanski", "mail@mail.com")
 const contact1 = new Contact("kamil", "rozanski", "mail@mail.com")
-contact.changeName("Jakil")
-// console.log(contact.creationDate, contact.id)
-console.log(contact, contact1)
 
 
 class Group {
@@ -44,8 +46,18 @@ class Group {
     constructor() {
         this.contacts = []
         this.group = []
+        this.groupID = uuidv4()
+    }
+
+    addContact = (contact) => {
+        this.contacts.push(contact)
     }
 }
+
+const group = new Group()
+group.addContact(contact)
+
+console.log(group)
 
 class AddressBook {
     // Ma mieć: listę wszystkich kontaktów, listę grup kontaktów 
