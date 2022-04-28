1211f415ef2c233d58e44513256887f8ab632854
KamilRozanski <motomc1@gmail.com>
2022-04-28 15:32:14 +0200

    remove methods add to Group class

--- addressBook-medium/addressBook.js
+++ addressBook-medium/addressBook.js
@@ -10,14 +10,12 @@ class Contact {
     // Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji i utworzenia, uuid
     // Ma umożliwiać: aktualizację datę modyfikacji, pozwalac na modyfikację imienia, nazwiska oraz adresu email
     constructor(name, lastName, email) {
-        const date = () => {
-            return new Date()
-        }
+        const date = new Date()
 
         this.name = name
         this.lastName = lastName
         this.email = email
-        this.creationDate = `${date().getDate()}/${date().getMonth() + 1}/${date().getFullYear()}`
+        this.creationDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
         this.modificationDate = false
         this.id = uuidv4()
     }
@@ -41,31 +39,51 @@ class Contact {
 class Group {
     // Ma mieć: listę kontaktów oraz nazwę grupy oraz uuid
     // Ma umożliwiać: zmianę nazwy grupy, można dodać lub usunac kontakt z grupy, można sprawdzić czy kontakt istnieje w grupie
-    constructor() {
-        this.contacts = []
+    constructor(groupName) {
         this.group = []
+        this.groupName = groupName
         this.groupID = uuidv4()
     }
 
     addContact = (contact) => {
-        this.contacts.push(contact)
+        this.group.push(contact)
+    }
+
+    removeContact = (contactName) => {
+        this.group = this.group.filter(el => {
+            el.name !== contactName;
+        })
+    }
+
+
+    hasContactExistInGroup = (contact) => {
+        // this.contacts.push(contact)
     }
 
     showAllContacts = () => {
-        return this.contacts
+        return this.group
     }
+
+    changeGroupName = (newName) => {
+        this.groupName = newName
+    }
+
 
 }
 
-const males = new Group()
-const females = new Group()
-males.addContact(new Contact("kamil", "rozanski", "mail@mail.com"))
-females.addContact(new Contact("dominika", "rozanska", "dominika@mail.com"))
-females.addContact(new Contact("Weronika", "rozanska", "weronika@mail.com"))
+const males = new Group("males")
+const females = new Group("females")
 
-// console.log(males)
-// console.log(females)
-console.log(females.showAllContacts())
+males.addContact(new Contact("Kamil", "rozanski", "mail@mail.com"))
+males.addContact(new Contact("Patryk", "rozanski", "mail@mail.com"))
+males.removeContact("Kamil")
+console.log(males.group)
+
+// females.addContact(new Contact("Dominika", "rozanska", "dominika@mail.com"))
+// females.addContact(new Contact("Weronika", "rozanska", "weronika@mail.com"))
+// console.log(males.changeGroupName("elo"))
+
+
 
 class AddressBook {
     // Ma mieć: listę wszystkich kontaktów, listę grup kontaktów 
