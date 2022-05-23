77da6b6b3559b4e19c021352a65969104e1307fa
KamilRozanski <motomc1@gmail.com>
2022-05-23 21:25:02 +0200

    practices with dependency inversion

--- addressBook-medium/addressBook.js
+++ addressBook-medium/addressBook.js
@@ -100,4 +100,63 @@ class AddressBook {
 }
 
 const addressBook = new AddressBook("elo")
-console.log(addressBook.show())
+// console.log(addressBook.show())
+
+
+
+class Store {
+    constructor(paymentProcessor) {
+        this.paymentProcessor = paymentProcessor
+    }
+
+    pay() {
+        return `${this.paymentProcessor.pay(2, 20)}`
+    }
+}
+
+class AllegroPay {
+    constructor(user) {
+        this.user = user
+    }
+    pay(quantity, price) {
+        return `${this.user} made a payment ${quantity * price} PLN`
+    }
+}
+
+class AllegroPaymentProcessor {
+    constructor(user) {
+        this.allegroPay = new AllegroPay(user)
+    }
+
+    pay(quantity, price) {
+        return this.allegroPay.pay(quantity, price)
+    }
+}
+
+
+class MBankPay {
+    constructor(user) {
+        this.user = user
+    }
+    pay(quantity, price) {
+        return `${this.user} made a payment by mBank ${(quantity * price) * 100} groszy`
+    }
+
+}
+class MBankPaymentProcessor {
+    constructor(user) {
+        this.mBank = new MBankPay(user)
+    }
+
+    pay(quantity, price) {
+        return this.mBank.pay(quantity, price)
+    }
+}
+
+
+
+
+
+
+const store = new Store(new MBankPay("kamil"))
+console.log(store.pay())
