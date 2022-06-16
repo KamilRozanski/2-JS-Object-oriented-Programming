c5d49ce9c54d0f97998644446e37fc0b58121bd0
KamilRozanski <motomc1@gmail.com>
2022-06-16 13:54:54 +0200

    Dynamic Switch app works

--- dynamicSwitch-easy/JS/Switch.js
+++ dynamicSwitch-easy/JS/Switch.js
@@ -17,18 +17,18 @@ export class Switch {
         })
     }
     isValid() {
-        this.cases = this.cases.filter(el => {
+        const resultArr = this.cases.filter(el => {
             if (el.condition) {
                 el.callback()
                 return el
             }
         })
-        return this.cases.length === 0
+        this.cases = []
+        return resultArr.length === 0
     }
 
 
     isEmpty = () => {
-        // console.log(this.cases.length)
         return this.cases.length === 0
     }
 }
