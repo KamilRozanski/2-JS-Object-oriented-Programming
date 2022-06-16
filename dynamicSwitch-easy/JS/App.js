3e4e94b2c5613d78d8bb292cc7790f812e0a8cef
KamilRozanski <motomc1@gmail.com>
2022-06-16 13:40:02 +0200

    test

--- dynamicSwitch-easy/JS/App.js
+++ dynamicSwitch-easy/JS/App.js
@@ -5,18 +5,14 @@ import {
 const formChecker = new Switch();
 const testValue = "Hello Word"
 
-formChecker.add(typeof testValue !== "string", () => {
+formChecker.add(typeof testValue === "string", () => {
     console.error("Value is not a string")
 })
 formChecker.add(testValue.length < 5, () => {
     console.error("Value must have at least five characters")
 })
-formChecker.add(testValue === "Hello Word", () => {
-    console.error("Value is not a string")
-})
-formChecker.add(testValue.length > 5, () => {
-    console.error("Value must have at least five characters")
-})
 
 
-formChecker.isValid()
+console.log(formChecker.isEmpty() + " formEmpty")
+console.log(formChecker.isValid())
+console.log(formChecker.isEmpty() + " formEmpty")
--- dynamicSwitch-easy/JS/Switch.js
+++ dynamicSwitch-easy/JS/Switch.js
@@ -17,15 +17,18 @@ export class Switch {
         })
     }
     isValid() {
-        this.cases.find((obj, index) => {
-            console.log(index)
-            for (const el in obj) {
-                if (!obj.condition) {
-
-                    return obj.callback()
-                }
+        this.cases = this.cases.filter(el => {
+            if (el.condition) {
+                el.callback()
+                return el
             }
         })
+        return this.cases.length === 0
     }
 
+
+    isEmpty = () => {
+        // console.log(this.cases.length)
+        return this.cases.length === 0
+    }
 }
