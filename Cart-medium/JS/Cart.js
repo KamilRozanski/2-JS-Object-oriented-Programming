049f44094073444ca2a8b90f68f8a9eebacc8598
KamilRozanski <motomc1@gmail.com>
2022-07-04 22:13:28 +0200

    getCartSummary works, but I need to change something

--- Cart-medium/JS/Cart.js
+++ Cart-medium/JS/Cart.js
@@ -1,9 +1,43 @@
 import {
     CartItem
 } from "./CartItem.js";
+import {
+    Validator
+} from './Validator.js';
+import {
+    v4 as uuidv4
+} from 'uuid';
 
 // Ma mieć: uuid, listę wybranych przedmiotów, rabat % na koszyk, kod rabatowy
 // Ma umożliwiać: 
 // - dodawanie/usuwanie przedmiotów do/z koszyka
 // - zmianę ilości produktu w koszyku
 // - podliczać wartość koszyka uwzględniajac rabaty
+
+export class Cart {
+    constructor() {
+        this.cart = []
+        this.quantity = 0
+        this.id = uuidv4()
+    }
+
+    addToCart = (item) => {
+        Validator.isInstanceOf(item, CartItem)
+        this.cart.push(item)
+        this.quantity++
+    }
+    removeFromCart = (item) => {
+        Validator.isInstanceOf(item, CartItem)
+        this.cart = this.cart.filter(el => el.id !== item.id)
+        this.quantity--
+    }
+    getCartSummary = () => {
+        return this.cart.reduce((acc, price, index) => {
+            price = this.cart[index].price
+            return acc += price - this.cart[index].discount
+        }, 0)
+    }
+    setDiscoundCode = (code, amountOfDiscount) => {
+
+    }
+}
--- Cart-medium/JS/Shop.js
+++ Cart-medium/JS/Shop.js
@@ -1,10 +1,20 @@
 import {
     CartItem
 } from './CartItem.js';
+import {
+    Cart
+} from './Cart.js';
 
 
-const phones = new CartItem("Samsung", "mobile phones", 100, 10)
-console.log(phones.getProcentageDiscount())
+const s21Ultra = new CartItem("Samsung S21 Ultra", "mobile phones", 5700, 199)
+const iPhone13ProMax = new CartItem("iPhone13 Pro Max", "mobile phones", 5800, 49)
+const redmiNote5A = new CartItem("Redmi Note 5A", "mobile phones", 1200, 49)
 
-phones.changePrice(1)
-console.log(phones)
+const cart = new Cart
+cart.addToCart(s21Ultra)
+cart.addToCart(iPhone13ProMax)
+cart.addToCart(redmiNote5A)
+console.log(cart.getCartSummary())
+
+
+// console.log(cart)
